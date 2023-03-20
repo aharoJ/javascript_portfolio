c83eb5fdc2fbc72de5805c22e747381c71e65ee1
Angel J. Haro <haro.j.angel@gmail.com>
2023-03-19 18:56:13 -0700

    removed boiler stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+
