55c0aab590792d9c501e842fc4790af73f962932
Angel J. Haro <haro.j.angel@gmail.com>
2023-03-27 13:16:41 -0700

    tails init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
