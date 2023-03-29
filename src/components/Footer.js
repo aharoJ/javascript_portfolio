c59aa821b089bc84e115d4e41db1460de8b95260
Angel J. Haro <haro.j.angel@gmail.com>
2023-03-29 16:09:01 -0700

    expirimenting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,14 @@ const Footer = () => {
         <Layout className='py-8 flex items-center justify-between '>
             <span> {new Date().getFullYear()} &copy; All Rights Reserved.</span>
             <div className='flex items-center'>
-            <Link href='https://aharoj.io' className='rounded-full underline-offset-4'> build with ...(<span className='text-primary text-2xl px-1'>&hearts; p &#9825;</span>)...</Link>
+            <Link href='https://aharoj.io' target={'_blank'}className='rounded-full underline-offset-4'> build with ...(<span className='text-primary text-2xl px-1'>&#9825; p &#9825;</span>)...</Link>
             </div>
             
-            <Link href='https://aharoj.io'className=''> aharoJ </Link>
+            <Link href='https://aharoj.io'className='' target={'_blank'}> aharoJ </Link>
         </Layout>
     </footer>
   )
 }
+
 
 export default Footer
