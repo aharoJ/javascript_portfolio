d4196f6558f21a87a841b2e2928fb157a80916db
Angel J. Haro <haro.j.angel@gmail.com>
2023-04-22 00:06:31 -0700

    external linking re-routing

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -10,17 +10,14 @@ const HireMe = () => {
     ">
       <div className="w-48 h-auto flex items-center justify-center relative">
         <CircularText className="text-black dark:text-white animate-spin-slow" />
-            <Link href='mailto:haro.j.angel@gmail.com' 
+            {/* <Link href='mailto:haro.j.angel@gmail.com'  */}
+            <Link href='https://github.com/aharoJ' 
             className='flex items-center justify-center 
             absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark
             text-light shadow-md border border-solid border-dark w-16 h-16 rounded-full
             font-semibold hover:bg-light hover:text-dark
             dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
             md:w-14 md:h-14 md:text-[12px]
-            
-            
-            
-      
             '>
               aharoJ 
             </Link>
