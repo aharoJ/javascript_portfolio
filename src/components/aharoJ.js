93c5a5ab30090713a3e1f7c192d8e536d28af580
Angel J. Haro <haro.j.angel@gmail.com>
2023-04-22 00:22:17 -0700

    ...aharoJ

--- src/components/aharoJ.js
+++ src/components/aharoJ.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import { CircularText } from './Icons'
 
-const HireMe = () => {
+const aharoJ = () => {
   return (
     <div className="fixed left-4 bottom-4 
     flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
@@ -11,7 +11,7 @@ const HireMe = () => {
       <div className="w-48 h-auto flex items-center justify-center relative">
         <CircularText className="text-black dark:text-white animate-spin-slow" />
             {/* <Link href='mailto:haro.j.angel@gmail.com'  */}
-            <Link href='https://github.com/aharoJ' 
+            <Link href='https://github.com/aharoJ' target={'_blank'}
             className='flex items-center justify-center 
             absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark
             text-light shadow-md border border-solid border-dark w-16 h-16 rounded-full
@@ -29,4 +29,4 @@ const HireMe = () => {
   )
 }
 
-export default HireMe
+export default aharoJ
