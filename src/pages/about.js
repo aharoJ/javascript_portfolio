25262ac12142ef68faec5c1d041a8f69f403e921
Angel J. Haro <haro.j.angel@gmail.com>
2024-01-03 02:00:01 -0800

    aharoJ...

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,122 +1,109 @@
-import AnimatedText from '@/components/AnimatedText'
-import Layout from '@/components/Layout'
-import Head from 'next/head'
-import Image from 'next/image'
-import React, { useRef, useEffect } from 'react'
-import profile_pic from 'public/images/aharoJ.jpg';
-import { useInView, useMotionValue, useSpring } from 'framer-motion'
-import Skills from '@/components/Skills'
-import Expirience from '@/components/Expirience'
-import Education from '@/components/Education'
-import Research from '@/components/Research'
-import TransitionEffect from '@/components/TransitionEffect'
-
-
-const AnimateNumbers= ({value})=>
-{
-    const ref= useRef(null);
-
-    const motionValue= useMotionValue(0);
-    const springValue= useSpring(motionValue, { duration:8000})
-    const isInView= useInView(ref, {once: true})
-
-
-    useEffect(() => 
-    {
-        if (isInView){
-            motionValue.set(value)
-        }
-        
-    }, [isInView, value, motionValue])
-
-
-    useEffect(() => {
-        springValue.on("change", (latest)=> {
-            if (ref.current && latest.toFixed(0) <= value){
-                ref.current.textContent= latest.toFixed()
-                }
-        })
-    }, [springValue, value])
-    
-    
-
-    return <span ref={ref}></span>
-}
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import profile_pic from 'public/images/profile/aharoJ.jpg';
+import TransitionEffect from '@/components/Animation/TransitionEffect';
+import Circular from '@/components/Animation/SpinningCircleAharoJ';
+import CoreSkills from '../components/CoreSkills'; 
+import BulletPoints from '@/components/abstract/BulletPoints';
+import Work from '@/components/my_experience/Work';
+import NonTechnical from '@/components/my_experience/NonTechnical';
+import Education from '@/components/my_experience/Education';
+import selfie from 'public/images/profile/selfie.png'
 
 
 const About = () => {
-  return (
-    <>
-        <Head>
-            <title> aharoJ | About </title>
-            <meta name="description" content="Created by aharoJ" />
-        </Head>
-        <TransitionEffect/>
+    return (
+<>
+    <Head>
+      <title>aharoJ | About</title>
+      <meta name="description" content="Created by aharoJ" />
+    </Head>
+
+    <TransitionEffect /> 
+
+    <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
+      <Layout className='pt-6 lg:p-24 md:pt-16 sm:pt-8'>
+        <div className=' mt-12 flex  w-full lg:flex-col'>
 
 
-        
-        <main className='flex w-full flex-col items-center justify-center dark:text-light'>
-            <Layout className='pt-16 '>
-            <AnimatedText text='aharoJ' className='mb-16 lg:!text-7xl sm:text-6xl xs:text-4xl sm:mb-8 !normal-case !tracking-tighter !font-light'/>
-            <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
-                <div className='col-span-3 flex flex-col items-start justify-center xl:col-span-4 md:order-2 md:col-span-8 '>
-                    <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'> Biography </h2>
-                        <p className='font-normal '> <span className=' font-bold text-3xl m-2'> About </span> Open-source advocate and have contributed to numerous open-source projects like Alacritty, Starship, Vivaldi and undertaken real-world projects like a Home-value predictor and a Capstone project for Machine-Learning. Currently working on 2 backend projects such as Twitter.io and Apple.io </p>
-                        <p className='font-normal'> <span className=' font-bold text-3xl m-2'> Non-Tech</span>I enjoy physical activities such as the gym, boxing, soccer. A fan of Anime, my picks Vinland - FMA - AOT. I also enjoy editing videos, images, and producing Youtube videos.</p>
-                        <p className='font-normal '> <span className=' font-bold text-3xl m-2'> Aspiration </span> I plan to pursue a PhD in computer science and become a professor where I can share my knowledge and passion for technology with future generations. </p>
-                </div>
-                <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark hover:border-4 hover:border-pink-900 
-                bg-light p-8 dark:bg-dark dark:hover:border-pink-900 xl:col-span-4 md:order-1 md:col-span-8 
-                '>
-                    <div className=' absolute top-0 -right-3 -z-20 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light/60'/>
-                    <Image  src={profile_pic} alt='aharoJ' className=' w-full h-auto rounded-2xl'
-                    priority
-                    sizes="(max-width: 768px) 100vw,
-                    (max-width: 1200px) 50vw,
-                    33vw"
-                    />
-                </div>
+            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Selfie         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+            <div className='w-full p-2 flex flex-col md:mb-6'>
+              <div className='col-span-4 relative h-max w-11/12 rounded-2xl border-2 border-solid p-4 
+              xl:col-span-4 md:order-1 md:col-span-8 sm:col-span-12 
+              border-dark hover:border-4 hover:border-pink-900 bg-light dark:bg-dark dark:hover:border-pink-900 
+              '>
+                  <div className=' absolute justify-content top-0 -right-3 -z-20 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light/60'/>
+                  <Image  src={profile_pic} alt='aharoJ' className=' w-full h-auto rounded-2xl'
+                  priority
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw" />       
+                    <div className='justify-content self-start mt-2 lg:self-center'>
+                          {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Resume         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+                          <Link href='/resume_angel_haro.pdf' target={'_blank'}
+                          className='flex items-center  bg-dark text-light p-1.5 px-6 rounded-lg
+                          hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark
+                          dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:border-light
+                          hover:dark:text-light md:p-2 md:mx-4
+                          text-base xs:text-sm xs:p-0.5
+                          '
+                          >  Resume 
+                          </Link> 
 
 
-
-                <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
-                    <div className='flex flex-col items-end justify-center xl:items-center'>
-                        <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                            <AnimateNumbers value={25}/>
-                        </span>
-                        <h2 className='text-lg font-light capitalize text-dark/75 tracking-wider dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                            Students | Mentees helped
-                        </h2>
+                          <Link href='mailto:haro.j.angel@gmail.com' target={'_blank'}
+                          className='flex items-center mt-2 bg-gray-800 text-light p-1.5 px-6 rounded-lg
+                          hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark
+                          dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:border-light
+                          hover:dark:text-light md:p-2 md:mx-4
+                          text-base xs:text-sm xs:p-0.5
+                          '
+                          > 
+                          Contact 
+                          </Link>           
                     </div>
-                    <div className='flex flex-col items-end justify-center xl:items-center'>
-                    <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                            <AnimateNumbers value={15}/>
-                        </span>
-                        <h2 className='text-lg font-light capitalize text-dark/75 tracking-wider dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                            Projects Completed
-                        </h2>
-                    </div>
-                    <div className='flex flex-col items-end justify-center xl:items-center'>
-                    <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                            <AnimateNumbers value={3}/>
-                        </span>
-                        <h2 className='text-lg font-light capitalize text-dark/75 tracking-wider dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                            Years of Experience 
-                        </h2>
-                    </div>
-                </div>
-
-
-
+              </div>
             </div>
-            <Skills/>
-            <Expirience/>
-            {/* <Education/> */}
-            <Research/>
-            </Layout>
-        </main>
-    </>
-  )
-}
+            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       ...........         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */} 
 
+
+            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Languages | Hobbies         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+            <div className='w-full p-2 flex flex-col md:flex-row md:p-0 md:items-start md:justify-between md:flex-wrap'>
+              <BulletPoints className="mb-4" title="Java" values={["Spring-Boot", "Spring-Security", "Maven"]} />
+              <BulletPoints className="mb-4" title="Shell" values={["Script Automation", "Unix-based Scripting", "YAML", "TOML"]} />
+            </div>
+            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       ...........         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */} 
+
+
+            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Physical Activities | Anime Enthusiast          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+            <div className='w-full p-2 flex flex-col md:flex-row md:p-0 md:items-start md:justify-between md:flex-wrap'>
+              <BulletPoints className="mb-4" title="Python" values={["Django", "TensorFlow", "Pandas", "Seaborn", "NumPy", "Matplotlib"]} />
+              <BulletPoints className="mb-4" title="JS & TS" values={["ReactJs", "NextJs", "TailwindCSS", "Bootstrap", "Framer-Motion"]} />
+            </div>
+            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       ...........         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */} 
+        
+        
+        
+        </div>
+        {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       ...........         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */} 
+
+        <Education/>
+        <Work/>
+        <NonTechnical/>
+        <CoreSkills />
+        {/* <Circular/> */}
+      </Layout>
+    </main>
+</>
+    )
+  }
 export default About
+
+{/* <BulletPoints className="mb-4" title="Physical Activities" values={["Gym", "Soccer", "Boxing", "Skateboarding"]} />
+                  
+                  <BulletPoints className="mb-4" title="Anime Enthusiast" values={["Cyberpunk: Edgerunners", "Fullmetal Alchemist", "Attack of Titans", "Demon Slayer", "Psycho-Pass"]} /> */}
+                            {/* <BulletPoints className="mt-12" title="Languages" values={["Java, Shell Scripting, Python", "YAML", "TOML", "HTML & CSS", "JavaScript", "TypeScript"]} />
+                  <BulletPoints className="mt-12" title="Hobbies" values={["Binging Anime", "Playing Video Games", "Configuring Dotfiles", "Programming Projects"]} /> */}
