e5931cb2587a513a2e64656f7b2314765cc3b5ff
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-08 12:45:19 -0700

    changed to standard double quotes

--- src/components/Animation/TextAnimation.js
+++ src/components/Animation/TextAnimation.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 const quote = {
   initial: {
@@ -25,19 +25,19 @@ const singleWord = {
   },
 };
 
-const TextAnimation = ({ text, className = '' }) => {
+const TextAnimation = ({ text, className = "" }) => {
   return (
-    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden sm:py-0 '>
+    <div className="mx-auto flex w-full items-center justify-center overflow-hidden py-2 text-center sm:py-0 ">
       <motion.h1
-        className={`inline-block w-full text-black font-bold capitalize text-2xl dark:text-light ${className}`}
+        className={`inline-block w-full text-2xl font-bold capitalize text-black dark:text-light ${className}`}
         variants={quote}
-        initial='initial'
-        animate='animate'
+        initial="initial"
+        animate="animate"
       >
-        {text.split(' ').map((word, index) => (
+        {text.split(" ").map((word, index) => (
           <motion.span
-            key={word + '-' + index}
-            className='inline-block'
+            key={word + "-" + index}
+            className="inline-block"
             variants={singleWord}
           >
             {word}&nbsp;
