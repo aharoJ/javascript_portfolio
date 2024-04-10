4191e31317a4231bb250a152575bd26dcfec6a1e
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-10 02:09:09 -0700

    integration of mobile and desktop... Testing will modularize code soon

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ColorfulAharoJHover from 'src/components/animation/ColorfulAharoJHover.js';
-import Desktop from './DesktopNav.js';
-import Mobile from './MobileNav.js';
+import React from "react";
+import ColorfulAharoJHover from "src/components/animation/ColorfulAharoJHover.js";
+import Desktop from "./DesktopNav.js";
+import Mobile from "./MobileNav.js";
+import LightDarkModeSwticherIcon from "../hooks/LightDarkModeSwitcherIcon.js";
 
 const Navbar = () => {
-    return (
-        <header className='sticky top-0 w-full px-0 py-0 font-medium flex items-center justify-between z-10 lg:px-16 md:px-6 md:py-5   
-        dark:text-light backdrop-blur-sm  text-dark bg-white dark:bg-black bg-opacity-50 dark:bg-opacity-50 '>
-            
-            <Desktop /> 
-            <Mobile />
-            <div className='absolute left-[50%] top-0 translate-x-[-50%]'>
-                <ColorfulAharoJHover/>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header
+      className="sticky top-0 z-10 flex w-full items-center justify-between bg-white bg-opacity-50 px-0 py-0 font-medium text-dark   
+        backdrop-blur-sm dark:bg-black  dark:bg-opacity-50 dark:text-light lg:px-16 md:px-6 md:py-5 "
+    >
+      <Desktop />
+      <Mobile />
+      <div className="absolute left-[50%] top-0 translate-x-[-50%]">
+        <ColorfulAharoJHover />
+      </div>
 
+      <div className="hidden md:absolute md:left-[85%] md:top-0 md:inline-block md:translate-x-[-50%]">
+        <LightDarkModeSwticherIcon />
+      </div>
+    </header>
+  );
+};
 
-
-
-
-
-export default Navbar
+export default Navbar;
