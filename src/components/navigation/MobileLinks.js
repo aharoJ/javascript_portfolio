65db4408286068f1f75ea4a582759a491afc3c5e
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-10 02:07:44 -0700

    extracted component for / /about /proj

---
+++ src/components/navigation/MobileLinks.js
@@ -0,0 +1,32 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const MobileLinks = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+  return (
+    <button
+      href={href}
+      className={`${className} group relative p-0 my-2 text-light dark:text-dark`}
+      onClick={handleClick}
+    >
+      {title}
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Hover          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+      <span
+        className={`
+          ease absolute -bottom-0 left-0
+          inline-block h-[1px] bg-orange-200
+          transition-[width] duration-500 group-hover:w-full dark:bg-gray-400
+          ${router.asPath === href ? "w-3/5" : "w-0"}
+                `}
+      >
+        &nbsp;
+      </span>
+    </button>
+  );
+};
+
+export default MobileLinks;
