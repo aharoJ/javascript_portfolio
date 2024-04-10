9fe955cb27f0bbe2853a288b5a15856fc1967035
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-10 02:05:23 -0700

    svg animated icon + new padding

--- src/components/navigation/DesktopNav.js
+++ src/components/navigation/DesktopNav.js
@@ -1,78 +1,79 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import React, { useState, useEffect } from 'react'
-import { SunIcon, MoonIcon  } from '../icons/SvgIcons'
-import { motion, useAnimation } from 'framer-motion';
-import useThemeSwitcher from '../hooks/useThemeSwitcher.js'
-import SocialMediaLinks from '../icons/SocialMediaIcons';
-import LightDarkButton from '../icons/MoonSunIcon';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
+import { SunIcon, MoonIcon } from "../icons/SvgIcons";
+import { motion, useAnimation } from "framer-motion";
+import useThemeSwitcher from "../hooks/useThemeSwitcher.js";
+import SocialMediaLinks from "../icons/SocialMediaIcons";
+import LightDarkButton from "../icons/MoonSunIcon";
 
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  return (
+    <Link href={href} className={`${className} group relative `}>
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
 
-
-const CustomLink= ({href, title, className=''})=> {
-    const router= useRouter();
-    return(
-        <Link href={href} className={`${className} relative group `}>
-{/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
-
-            {title}
-            <span className={`
-                h-[1px] inline-block bg-dark
-                absolute left-0 -bottom-0.5
-                group-hover:w-full transition-[width] ease duration-500
-                ${router.asPath === href ? 'w-full' : 'w-0'}
+      {title}
+      <span
+        className={`
+                ease absolute -bottom-0.5
+                left-0 inline-block h-[1px]
+                bg-dark transition-[width] duration-500 group-hover:w-full
+                ${router.asPath === href ? "w-full" : "w-0"}
                 dark:bg-light
                 `}
-            >
-                &nbsp;
-            </span>
-        </Link>
-    )
-}
-
-
-
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 function useForceUpdate() {
-    const [value, setValue] = useState(0);
-    return () => setValue(value + 1);
+  const [value, setValue] = useState(0);
+  return () => setValue(value + 1);
 }
 
 const DesktopNav = () => {
-    const [mode, setMode] = useThemeSwitcher();
-    const router = useRouter();
-    const forceUpdate = useForceUpdate();
+  const [mode, setMode] = useThemeSwitcher();
+  const router = useRouter();
+  const forceUpdate = useForceUpdate();
 
-    useEffect(() => {
-        router.events.on('routeChangeComplete', forceUpdate);
+  useEffect(() => {
+    router.events.on("routeChangeComplete", forceUpdate);
 
-        return () => {
-            router.events.off('routeChangeComplete', forceUpdate);
-        }
-    }, [router.events]);
+    return () => {
+      router.events.off("routeChangeComplete", forceUpdate);
+    };
+  }, [router.events]);
 
+  return (
+    <div
+      className="flex h-full w-full items-center justify-between bg-light/10 p-1
+        backdrop-blur-md dark:bg-dark/50 dark:backdrop-blur-md lg:hidden
+        "
+    >
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Home About           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+      <nav>
+        <CustomLink href="/" title="Home" className="m-6 text-2xl" />
+        <CustomLink href="/about" title="About" className="m-6 text-2xl" />
+        <CustomLink
+          href="/projects"
+          title="Projects"
+          className="m-6 text-2xl"
+        />
+        {/* Rest of your links here */}
+      </nav>
 
-    return (
-        <div className='w-full h-full flex justify-between items-center lg:hidden p-1
-        bg-light/10 backdrop-blur-md dark:bg-dark/50 dark:backdrop-blur-md
-        '>
-            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Home About           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
-            <nav>
-                <CustomLink href='/' title='Home' className='m-6 text-2xl' />
-                <CustomLink href='/about' title='About' className='m-6 text-2xl' />
-                <CustomLink href='/projects' title='Projects' className='m-6 text-2xl' />
-                {/* Rest of your links here */}
-            </nav>
-
-            {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       ICONS for Desktop          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
-            <nav className='flex items-center justify-center'>
-                <SocialMediaLinks size={30} />      {/* Optional: classname, size, mobilesize */}
-                <LightDarkButton size={30} className="p-0"/>         {/* Optional classname, size, mobilesize */}
-                
-            </nav>
-            
-        </div>
-    )
-}
+      {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       ICONS for Desktop          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+      <nav className="flex items-center justify-center">
+        <SocialMediaLinks size={30} />{" "}
+        {/* Optional: classname, size, mobilesize */}
+        {/* <LightDarkButton size={30} className="p-0" />{" "} */}
+        {/* Optional classname, size, mobilesize */}
+      </nav>
+    </div>
+  );
+};
 
 export default DesktopNav;
