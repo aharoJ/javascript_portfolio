7320b87859d1e18143f7a75e61ce2d64ac8677a5
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-09 19:04:01 -0700

    stripped a phone v. desktop versions for optimization

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,12 +20,19 @@ const About = () => {
         <Layout className="p-0 lg:p-24 md:pt-16 sm:pt-8">
           {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       SELFIE | DOUGHNUT          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
           <div className="flex w-full">
-            <div className="flex w-2/5 justify-center p-2  ">
+            <div className="flex w-2/5 justify-center p-2 md:hidden ">
               <VerticalSelfie />
             </div>
-            <div className="flex w-3/5 items-center p-2">
+            <div className="flex w-3/5 items-center p-2 md:hidden ">
               <DoughnutChart />
             </div>
+          </div>
+          {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       iPHONE: SELFIE | DOUGHNUT          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
+          <div className="hidden lg:hidden md:flex md:w-[100%] md:flex-col">
+            <VerticalSelfie />
+          </div>
+          <div className="hidden lg:hidden md:mb-8 md:flex md:w-[100%] md:flex-col">
+            <DoughnutChart />
           </div>
           {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       ...........         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
 
