c36d2c1f63d3c1606be1fdfe2e7f4a1c8c7a0dfe
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-12 01:27:14 -0700

    new padding, new colors, new component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import TransitionEffect from "@/components/animation/TransitionEffect";
 import HorizantalSelfie from "@/components/experience/HorizantalSelfie";
-import ResumeContact from "@/components/button/ResumeContact";
 import AnimatedStats from "@/components/animation/AnimatedStats";
 import LandingProject from "@/components/experience/LandingProject";
+import ResumeContactButton from "@/components/button/ResumeContactButton";
 
 const Home = () => {
   const alan = `Until you make the unconscious conscious, it will direct your life and you will call it fate. You are a function of what the whole universe is doing in the same way that a wave is a function of what the whole ocean is doing. - Watts`;
@@ -28,14 +28,14 @@ const Home = () => {
             <div className="flex w-[63%] flex-col items-center self-center md:w-full">
               <TextAnimation
                 text="Angel J. Haro"
-                className="mb-2 justify-center text-8xl font-light tracking-wider text-gray-400 
+                className="mb-2 justify-center text-9xl font-light tracking-wider text-gray-400 
                 dark:text-orange-200 md:mb-8 md:flex-wrap md:text-5xl md:font-light md:!normal-case"
               />
               <div className="flex w-[90%] items-center self-center md:flex-wrap md:mb-8">
-                <p className="text-xl font-normal md:font-light">{alan} </p>
+                <p className="text-3xl font-normal md:font-light">{alan} </p>
               </div>
               <div className="flex w-[50%] md:w-[100%] my-4">
-                <ResumeContact />
+                <ResumeContactButton />
               </div>
             </div>
           </div>
