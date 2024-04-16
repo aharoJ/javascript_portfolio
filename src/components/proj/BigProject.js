bd544b76d10984538e38ff4746d801874d075127
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-16 13:17:39 -0700

    personal notes for now

--- src/components/proj/BigProject.js
+++ src/components/proj/BigProject.js
@@ -15,8 +15,8 @@ const BigProject = ({
 }) => {
   return (
     <article
-      className="relative flex w-full flex-wrap items-center
-        rounded-3xl border border-solid border-dark bg-light p-2 dark:border-light dark:bg-dark
+      className="relative flex w-full  items-center
+        rounded-3xl border border-solid border-dark bg-light p-4 dark:border-light dark:bg-dark
         lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
     >
       <div
@@ -48,40 +48,31 @@ const BigProject = ({
         {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       CONTENT         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
         <div className="flex w-[50%] flex-col md:w-full">
           {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       TAGS         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
-          <div className="ml-6 flex w-full justify-start">
-            <div className="space-x-4 md:hidden">
-              {hashtags.map((tag, index) => (
-                <span
-                  key={index}
-                  className="text-2xl font-normal  text-dark/80 dark:text-light/80
-                  md:text-sm md:font-light md:tracking-tighter "
-                >
-                  #{tag}
-                </span>
-              ))}
-            </div>
+          <div className="ml-6 flex flex-wrap justify-start space-x-4">
+            {hashtags.map((tag, index) => (
+              <span
+                key={index}
+                className="text-3xl font-normal  text-dark/80 dark:text-light/80"
+              >
+                #{tag}
+              </span>
+            ))}
           </div>
-
           {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Title | Description         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
           <div className="ml-4 flex w-full flex-col ">
-            <Link
-              href={link}
-              target="_blank"
-              className="underline-offset-2 hover:underline"
-            >
-              <h2 className="my-2  text-left text-8xl font-semibold text-gray-400 dark:text-orange-200 sm:text-sm">
+            <Link href={link} target="_blank">
+              <h2 className="my-2  text-left text-9xl font-semibold text-gray-400 dark:text-orange-200 sm:text-sm">
                 {title}
               </h2>
             </Link>
 
-            <p className="my-2 flex-wrap text-3xl font-medium text-dark dark:text-light sm:text-sm">
+            <p className="my-2 px-2 text-3xl font-medium text-dark dark:text-light sm:text-sm">
               {description}
             </p>
           </div>
-
           {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       Icon | Visit Project         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
           <div className="mt-2 flex w-full items-center justify-end">
-            <Link href={github} target="_blank" className="w-14">
+            <Link href={github} target="_blank" className="w-16">
               <GithubIcon />
             </Link>
             <Link
@@ -90,7 +81,7 @@ const BigProject = ({
               className="ml-4 rounded-lg bg-dark p-2 px-4 text-2xl font-semibold text-light 
                 dark:bg-light dark:text-dark sm:px-4 sm:text-base"
             >
-              Visit the Project
+              Visit Project
             </Link>
           </div>
         </div>
