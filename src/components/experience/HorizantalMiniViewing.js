cbd736f14bd06560b1c428b6d692114c41f428f8
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-18 15:05:06 -0700

    new padding all around

--- src/components/experience/HorizantalMiniViewing.js
+++ src/components/experience/HorizantalMiniViewing.js
@@ -8,11 +8,11 @@ const HorizantalMiniViewing = ({ img, link }) => {
   return (
     <article
       className="relative flex w-[63%] flex-col items-center justify-center rounded-2xl border 
-      border-solid border-dark bg-light p-4 dark:border-light dark:bg-dark md:p-2"
+      border-solid border-dark bg-light p-6 dark:border-light dark:bg-dark md:p-2"
     >
       <div
-        className="absolute -right-2 top-0 -z-10 h-[101%] w-[101%] rounded-[2.5rem] rounded-br-3xl 
-        bg-dark dark:bg-light md:h-[102%] md:w-[101%] md:rounded-br-xl"
+        className="absolute -right-2.5 top-0 -z-10 h-[101%] w-[101%] rounded-[1rem] rounded-br-3xl 
+        bg-dark/80 dark:bg-light md:h-[102%] md:w-[101%] md:rounded-br-xl"
       />
 
       <Link
@@ -29,18 +29,18 @@ const HorizantalMiniViewing = ({ img, link }) => {
         />
       </Link>
 
-      <div className="mt-2 w-full self-start lg:self-center">
+      <div className="mt-4 w-full self-center lg:self-center">
         <Link
           href="https://aharoj.io/viewing"
           target={"_blank"}
-          className="mt-2 flex items-center justify-center rounded-lg border border-solid border-transparent
-            bg-gray-800 p-0 text-base text-light hover:border-dark hover:bg-light
+          className="flex items-center justify-center rounded-lg border border-solid border-transparent
+            bg-gray-800 p-1 text-base text-light hover:border-dark hover:bg-light
             hover:text-dark dark:bg-gray-400 hover:dark:border-light
-            hover:dark:bg-dark hover:dark:text-light md:mx-4
+            hover:dark:bg-light hover:dark:text-dark/80 md:mx-4
             md:p-2 xs:p-0.5 xs:text-sm"
         >
-          <p className="text-2xl font-extrabold tracking-tight">
-            Visit All OPENSOURCE Projects
+          <p className="text-5xl font-extrabold tracking-wide ">
+            VISIT All PROJECTS
           </p>
         </Link>
       </div>
