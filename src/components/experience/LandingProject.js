e644b2d6b7010d730c8b23354735333ee9641783
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-19 23:35:27 -0700

    landing project

--- src/components/experience/LandingProject.js
+++ src/components/experience/LandingProject.js
@@ -2,21 +2,24 @@ import React from "react";
 import AnimatedText from "@/components/animation/TextAnimation";
 import FeaturedProject from "@/components/abstract/FeaturedProject";
 import project from "public/images/projects/opensource-portfolio.png";
+import BigProject from "../proj/BigProject";
 
 const LandingProject = () => {
   return (
     <>
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       OpenSource Portfolio         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */}
       <AnimatedText
-        text="Open Source Portfolio"
-        className=" tracking mb-8 mt-3 text-6xl font-normal text-gray-400 dark:text-orange-200 sm:mb-8 sm:!text-6xl xs:!text-4xl"
+        text="Open Source "
+        className=" tracking mb-8 mt-3 text-8xl font-normal text-gray-400 dark:text-orange-200 sm:mb-8 sm:!text-6xl xs:!text-4xl"
       />
       <div className="grid grid-cols-12 gap-x-8 gap-y-8 md:gap-x-6 md:gap-y-10">
         <div className="col-span-12">
-          <FeaturedProject
+          <BigProject
             title="Portfolio "
             img={project}
-            summary="All code for the frontend is available in GitHub and all backend is available trough the a zip."
+            description="Built with Next.js and styled by TailwindCSS, a custom light-dark mode switcher. 
+            Dynamic UI for Iphones and Macbooks. React Icons, a custom animated SVG sun/moon toggler. 
+            Motion-Framer for animations. Backed with Spring-Boot and Spring-Security and PostgreSQL for database managment. All backend is available trough a zip."
             github="https://github.com/aharoJ/portfolio"
             link="https://github.com/aharoJ/portfolio"
             hashtags={["NextJs", "Spring-Boot", "TailwindCSS", "PostgreSQL"]}
