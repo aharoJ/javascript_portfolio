63890240d2082ef9848daeb39cbcfd77f616c8b9
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-20 18:06:28 -0700

    new font size dimensions

--- src/components/abstract/ExperienceInfo.js
+++ src/components/abstract/ExperienceInfo.js
@@ -7,7 +7,7 @@ const ExperienceInfo = ({ title, company, bullets }) => {
   return (
     <li
       ref={listItemRef}
-      className="mx-auto mb-12 flex w-[90%] flex-col items-center justify-between first:mt-0 last:mb-0 md:w-[80%]"
+      className="mx-auto mb-24 flex w-[90%] flex-col items-center justify-between first:mt-0 last:mb-0 md:w-[85%]"
     >
       <ScrollTimeLineAnimation reference={listItemRef} />
 
@@ -16,9 +16,9 @@ const ExperienceInfo = ({ title, company, bullets }) => {
         whileInView={{ y: 0, x: 0 }}
         transition={{ duration: 1.5, type: "spring" }}
       >
-        <h1 className="ml-4 text-2xl font-normal text-black/80 dark:text-white md:text-xl">
+        <h1 className="ml-4 text-3xl font-normal text-black/80 dark:text-white md:text-2xl">
           {title}&nbsp;
-          <span className="text-2xl font-light tracking-wide text-dark/80 dark:text-light/80 md:text-xl">
+          <span className="text-3xl font-light tracking-wide text-dark/80 dark:text-light/80 md:text-2xl">
             @{company}
           </span>
         </h1>
@@ -27,7 +27,7 @@ const ExperienceInfo = ({ title, company, bullets }) => {
           <ul className="mt-2 list-disc">
             {bullets.map((bullet, index) => (
               <li
-                className="ml-4 mt-2 text-xl font-normal text-dark/80 dark:text-light md:text-base"
+                className="ml-4 mt-2 text-2xl font-normal text-dark/80 dark:text-light md:text-lg"
                 key={index}
               >
                 {bullet}
